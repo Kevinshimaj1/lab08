c7494068b63173d3c0ff3b9ae44a017f547d2fbf
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2023-11-13 18:57:15 +0100

    Finished exercise 82-simple-gui

--- 82-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 82-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,8 +1,11 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -22,15 +25,21 @@ public class MiniGUI {
     private static final int PROPORTION = 5;
     private final Random randomGenerator = new Random();
     private final JFrame frame = new JFrame(TITLE);
+    private final JPanel panel = new JPanel();
+    private final JTextField textField = new JTextField("Result");
+
 
     /**
      * Creates a new {@link MiniGUI}.
      */
     public MiniGUI() {
+        final JButton write = new JButton("Print a random number on standard output");
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(write);
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
-        final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
+        canvas.add(panel,BorderLayout.CENTER);
+        canvas.add(textField,BorderLayout.NORTH);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /*
@@ -39,7 +48,9 @@ public class MiniGUI {
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                int copyInt = randomGenerator.nextInt();
+                System.out.println(copyInt);
+                textField.setText("Random number: "+copyInt);
             }
         });
     }
